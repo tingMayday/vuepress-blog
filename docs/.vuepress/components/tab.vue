7719eb533fac22bf2f44255c5091a50ebaf663c6
tingMayday <1144826491@qq.com>
2021-09-28 16:47:09 +0800

    修改主题和关于我

--- docs/.vuepress/components/tab.vue
+++ docs/.vuepress/components/tab.vue
@@ -2,17 +2,19 @@
   <div class="tab">
     <ul class="tab-menu">
       <li
-        v-for="(item,index) in tabMenu"
-        :key
-        :class="tabActive === index ? 'active':''"
+        v-for="(item, index) in tabMenu"
+        :key="index"
+        :class="tabActive === index ? 'active' : ''"
         @click="tabClick(index)"
-      >{{item}}</li>
+      >
+        {{ item }}
+      </li>
     </ul>
     <div class="tab-con">
       <div class="tab-0" v-if="tabActive === 0">
         <ul class="skill">
-          <li v-for="item in skills">
-            <div class="label">{{item.name}}</div>
+          <li v-for="(item, index) in skills" :key="index">
+            <div class="label">{{ item.name }}</div>
             <div class="bar">
               <div class="bar-line" :style="'width:' + item.percen + '%'"></div>
             </div>
@@ -21,34 +23,36 @@
       </div>
       <div class="tab-1" v-if="tabActive === 1">
         <ul class="works">
-          <li v-for="item in works">
-            <div>{{item.date}}</div>
-            <div>{{item.company}}</div>
-            <div>{{item.job}}</div>
+          <li v-for="(item, index) in works" :key="index">
+            <div>{{ item.date }}</div>
+            <div>{{ item.company }}</div>
+            <div>{{ item.job }}</div>
           </li>
         </ul>
       </div>
       <div class="tab-2" v-if="tabActive === 2">
         <div class="b-title">正在看的</div>
         <ul class="books">
-          <li v-for="item in books.reading">{{item}}</li>
+          <li v-for="(item, index) in books.reading" :key="index">
+            {{ item }}
+          </li>
         </ul>
         <div class="b-title">我喜欢的</div>
         <ul class="books">
-          <li v-for="item in books.like">{{item}}</li>
+          <li v-for="(item, index) in books.like" :key="index">{{ item }}</li>
         </ul>
         <div class="b-title">已看完的</div>
         <ul class="books">
-          <li v-for="item in books.read">{{item}}</li>
+          <li v-for="(item, index) in books.read" :key="index">{{ item }}</li>
         </ul>
       </div>
       <div class="tab-3" v-if="tabActive === 3">
         <ul class="games">
-          <li v-for="(item,index) in games">
+          <li v-for="(item, index) in games" :key="index">
             <div class="game-logo">
               <img :src="'/game' + (index + 1) + '.png'" alt />
             </div>
-            <div class="game-name">{{item}}</div>
+            <div class="game-name">{{ item }}</div>
           </li>
         </ul>
         <div class="game">
@@ -67,60 +71,68 @@ export default {
       tabMenu: ["我的技能", "工作经历", "我看的书", "玩的游戏"],
       tabActive: 0,
       skills: [
-        { name: "html", percen: 90 },
-        { name: "css", percen: 90 },
-        { name: "javascript", percen: 85 },
-        { name: "vue", percen: 80 },
-        { name: "electron", percen: 75 },
+        { name: "nodeJs", percen: 60 },
         { name: "小程序", percen: 80 },
-        { name: "nodeJs", percen: 60 }
+        { name: "electron", percen: 75 },
+        { name: "vue", percen: 80 },
+        { name: "javascript", percen: 85 },
+        { name: "html(html5)", percen: 90 },
+        { name: "css(css3)", percen: 90 },
+        { name: "photoshop", percen: 60 },
       ],
       works: [
         {
           date: "2018.07 ~ now",
           company: "DuGong",
-          job: "前端开发工程师/小程序"
+          job: "前端开发工程师/小程序",
         },
         {
           date: "2016.06 ~ 2018.04",
           company: "SouZhong",
-          job: " UI(设计组长)、 前端开发/小程序"
+          job: " UI(设计组长)、 前端开发/小程序",
         },
         {
           date: "2016.02 ~ 2016.06 ",
           company: "YuanChang",
-          job: "平面设计、前端开发"
-        }
+          job: "平面设计、前端开发",
+        },
       ],
       books: {
-        reading: ["我有一座冒险屋 - 我会修空调"],
-        like: ["侦探", "灵异", "文学", "心理学"],
+        reading: ["《小孩》 - 大冰"],
+        like: ["侦探", "灵异", "悬疑", "文学", "心理学"],
         read: [
-          "明朝那些事1 - 当年明月",
-          "人生百忌 - 刘墉",
-          "信 - 东野圭吾",
-          "问罪者 - 苍狼残歌",
-          "世界上所有的童话都是写给大人的 - 陈谌",
-          "特案侦查组 - 陈猛",
-          "猫眼 - 蔡俊",
-          "蝴蝶公墓 - 蔡俊",
-          "爱人的头颅 - 蔡俊",
-          "十宗罪2 - 蜘蛛",
-          "十宗罪3 - 蜘蛛",
-          "十宗罪4 - 蜘蛛",
-          "十宗罪5 - 蜘蛛",
-          "活着 - 余华",
-          "我当心理咨询师遇到的那些怪诞事件 - 胡定乐",
-        ]
+          "《我有一座冒险屋》 - 我会修空调",
+          "《尸案调查科》 - 九滴水",
+          "《悬案组》 - 独孤求剩",
+          "《明朝那些事1》 - 当年明月",
+          "《人生百忌》 - 刘墉",
+          "《信》 - 东野圭吾",
+          "《问罪者》 - 苍狼残歌",
+          "《世界上所有的童话都是写给大人的》 - 陈谌",
+          "《特案侦查组》 - 陈猛",
+          "《猫眼》 - 蔡俊",
+          "《蝴蝶公墓》 - 蔡俊",
+          "《爱人的头颅》 - 蔡俊",
+          "《十宗罪1》 - 蜘蛛",
+          "《十宗罪2》 - 蜘蛛",
+          "《十宗罪3》 - 蜘蛛",
+          "《十宗罪4》 - 蜘蛛",
+          "《十宗罪5》 - 蜘蛛",
+          "《活着》 - 余华",
+          "《我当心理咨询师遇到的那些怪诞事件》 - 胡定乐",
+          "《皮囊》 - 蔡崇达",
+          "《蔡康永的说话之道》 - 蔡康永",
+          "《蔡康永的说话之道2》 - 蔡康永",
+        ],
       },
-      games: ["和平精英", "明日之后", "王者荣耀"]
+      games: ["和平精英", "明日之后", "王者荣耀", "巫师3"],
     };
   },
   methods: {
     tabClick(idx) {
       this.tabActive = idx;
-    }
-  }
+    },
+  },
 };
 </script>
 
@@ -143,7 +155,7 @@ ul {
   li {
     flex: 1;
     padding: 0.5rem 0;
-    cursor pointer
+    cursor: pointer;
 
     &:hover, &.active {
       background: #f99;
@@ -155,6 +167,7 @@ ul {
 .tab-con {
   margin-top: 1rem;
   padding: 3rem;
+  background: #fff;
   box-shadow: 0 1px 8px 0 rgba(0, 0, 0, 0.3);
 
   .skill {
@@ -164,7 +177,7 @@ ul {
       padding: 1rem;
 
       .label {
-        width: 5.5rem;
+        width: 6rem;
       }
 
       .bar {
